33f43505a677fa048919c9d63fd105722be612f2
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-08-24 09:03:28 +0600

    feat: complete redux store and axios set up

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import { ThemeProvider } from "./providers/theme-provider.tsx";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
 import { Toaster } from "sonner";
+import { Provider as ReduxProvider} from "react-redux";
+import { store } from "./store/index.ts";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<RouterProvider router={router} />
-			<Toaster richColors />
-		</ThemeProvider>
+		<ReduxProvider store={store}>
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<RouterProvider router={router} />
+				<Toaster richColors />
+			</ThemeProvider>
+		</ReduxProvider>
 	</StrictMode>
 );
